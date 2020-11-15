dcb55f816286c47ec6157b522cc0cdc136e17413
kumarutsav7 <avecutsav111@gmail.com>
2020-11-15 10:58:36 +0530

    Javascript on its way

---
+++ home.js
@@ -0,0 +1,11 @@
+console.log('hello');
+//alert('This is Qazi');
+// how to write a comment inline performance
+//variables 
+var b = 'kumar';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById("someText").innerHTML = "Hey There";
